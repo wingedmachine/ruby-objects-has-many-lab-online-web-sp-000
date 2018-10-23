659ba73a826db9c8ef51491e11b743c7293b85a9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:02:08 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,12 @@
 class Author
   attr_accessor :name, :posts
 
+  AUTHORS = []
+
   def initialize(name)
     @name = name
     @posts = []
+    AUTHORS << self
   end
 
   def add_post(post)
@@ -14,5 +17,8 @@ class Author
   def add_post_by_title(title)
     add_post(Post.new(title))
   end
+
+  def self.post_count
+    AUTHORS.inject(0) { |sum, author| sum + author.posts.size }
+  end
 end
-  
