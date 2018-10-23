f769ad1228ab86576718af06fe7979c19e4e26a9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 20:47:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,4 +10,10 @@ class Artist
     @songs << song
     song.add_artist(self)
   end
+
+  def add_song_by_name(name)
+    new_song = song.new(name)
+    new_song.add_artist(self)
+    @songs << new_song
+  end
 end
